08fa9b5f4818823280edfb9bc5829acad99d9aa8
armany87 <h.gonzalezrivera2@gmail.com>
2021-07-27 17:35:16 -0500

    Add files via upload

--- basketball_app.py
+++ basketball_app.py
@@ -5,21 +5,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-st.title('NBA 2K League Player Stats Explorer')
+st.title('NBA Player Stats Explorer')
 
 st.markdown("""
-This app performs simple webscraping of NBA 2K Lweague player stats data!
+This app performs simple webscraping of NBA player stats data!
 * **Python libraries:** base64, pandas, streamlit
-* **Data source:** [https://2kleague.nba.com/stats/](https://2kleague.nba.com/stats//).
+* **Data source:** [Basketball-reference.com](https://www.basketball-reference.com/).
 """)
 
 st.sidebar.header('User Input Features')
-selected_year = st.sidebar.selectbox('Year', list(reversed(range(2018,2021))))
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2020))))
 
 # Web scraping of NBA player stats
 @st.cache
 def load_data(year):
-    url = "https://2kleague.nba.com/stats/"
+    url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
     html = pd.read_html(url, header = 0)
     df = html[0]
     raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content

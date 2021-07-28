8f6a32f80480b80d08602baef34f56e4a4a96710
armany87 <h.gonzalezrivera2@gmail.com>
2021-07-28 18:01:16 -0500

    Add files via upload

--- basketball_app.py
+++ basketball_app.py
@@ -22,7 +22,6 @@ def load_data(year):
     url = "https://2kleague.nba.com/stats/"
     html = pd.read_html(url, header = 0)
     df = html[0]
-    raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
     raw = raw.fillna(0)
     playerstats = raw.drop(['Rk'], axis=1)
     return playerstats
